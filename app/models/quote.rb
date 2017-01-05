7dbe1153f179e1894231eebcbbc4b72ccd918319
Jim X. Lin <miljinx@gmail.com>
2017-01-04 16:12:32 -0800

    Add about page

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,5 +1,5 @@
 class Quote < ApplicationRecord
   validates :saying, presence: true, length: { maximum: 140, minimum: 3 }
   validates :author, presence: true, length: { maximum: 50, minimum: 3 },
-            format: { with: /Terry Pratchett \(.+\)/, message "must be in the form: Terry Pratchett (Reference)" }
+            format: { with: /Terry Pratchett \(.+\)/, message: "must be in the form: Terry Pratchett (reference)" }
 end
