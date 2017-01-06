2299fb47270e8dccf8754f36a469897ee9cb5516
Jim X. Lin <miljinx@gmail.com>
2017-01-05 22:39:54 -0800

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,108 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Quote.create(saying: "The trouble with having an open mind, of course, is that people will insist on coming along and trying to put things in it.",
+             author: "Terry Pratchett (Diggers)")
+
+Quote.create(saying: "Give a man a fire and he's warm for a day, but set fire to him and he's warm for the rest of his life.",
+            author: "Terry Pratchett (Jingo)")
+
+Quote.create(saying: "It is said that your life flashes before your eyes just before you die. That is true, it's called Life.",
+            author: "Terry Pratchett (The Last Continent)")
+
+Quote.create(saying: "Time is a drug. Too much of it kills you.",
+            author: "Terry Pratchett (Small Gods)")
+
+Quote.create(saying: "In the beginning there was nothing, which exploded.",
+            author: "Terry Pratchett (Lords and Ladies)")
+
+Quote.create(saying: "Real stupidity beats artificial intelligence every time.",
+            author: "Terry Pratchett (Hogfather)")
+
+Quote.create(saying: "Just erotic. Nothing kinky. It's the difference between using a feather and using a chicken.",
+            author: "Terry Pratchett (Eric)")
+
+Quote.create(saying: "Coming back to where you started is not the same as never leaving.",
+            author: "Terry Pratchett (A Hat Full of Sky)")
+
+Quote.create(saying: "The presence of those seeking the truth is infinitely to be preferred to the presence of those who think they've found it.",
+            author: "Terry Pratchett (Monstrous Regiment)")
+
+Quote.create(saying: "This book was written using 100% recycled words.",
+            author: "Terry Pratchett (Wyrd Sisters)")
+
+Quote.create(saying: "...inside every old person is a young person wondering what happened.",
+            author: "Terry Pratchett (Moving Pictures)")
+
+Quote.create(saying: "Do you not know that a man is not dead while his name is still spoken?",
+            author: "Terry Pratchett (Going Postal)")
+
+Quote.create(saying: "Sometimes glass glitters more than diamonds because it has more to prove.",
+            author: "Terry Pratchett (The Truth)")
+
+Quote.create(saying: "Do you think it's possible for an entire nation to be insane?",
+            author: "Terry Pratchett (Monstrous Regiment)")
+
+Quote.create(saying: "The intelligence of that creature known as a crowd is the square root of the number of people in it.",
+            author: "Terry Pratchett (Jingo)")
+
+Quote.create(saying: "There isn't a way things should be. There's just what happens, and what we do.",
+            author: "Terry Pratchett (A Hat Full of Sky)")
+
+Quote.create(saying: "Humans need fantasy to be human. To be the place where the falling angel meets the rising ape.",
+            author: "Terry Pratchett (Hogfather)")
+
+Quote.create(saying: "Thunder rolled. ... It rolled a six.",
+            author: "Terry Pratchett (Guards! Guards!)")
+
+Quote.create(saying: "A good bookshop is just a genteel Black Hole that knows how to read.",
+            author: "Terry Pratchett (Guards! Guards!)")
+
+Quote.create(saying: "It's not enough to be able to pick up a sword. You have to know which end to poke into the enemy.",
+            author: "Terry Pratchett (Lords and Ladies)")
+
+Quote.create(saying: "This isn't life in the fast lane, it's life in the oncoming traffic.",
+            author: "Terry Pratchett (alt.fan.pratchett)")
+
+Quote.create(saying: "I didn't go to university. . . . But I have sympathy for those who did.",
+            author: "Terry Pratchett (alt.fan.pratchett)")
+
+Quote.create(saying: "If it wasn't for the fun and money, I really don't know why I'd bother.",
+            author: "Terry Pratchett (alt.fan.pratchett)")
+
+Quote.create(saying: "One day I'll be dead and THEN you'll all be sorry.",
+            author: "Terry Pratchett (alt.fan.pratchett)")
+
+Quote.create(saying: "What heroes like best is themselves.",
+            author: "Terry Pratchett (The Colour of Magic)")
+
+Quote.create(saying: "The only reason for walking into the jaws of Death is so's you can steal his gold teeth.",
+            author: "Terry Pratchett (The Colour of Magic)")
+
+Quote.create(saying: "The pen is mightier than the sword ... if the sword is very short, and the pen is very sharp.",
+            author: "Terry Pratchett (Light Fantastic)")
+
+Quote.create(saying: "They say a little knowledge is a dangerous thing, but it is not one half so bad as a lot of ignorance.",
+            author: "Terry Pratchett (Equal Rites)")
+
+Quote.create(saying: "You know, people just don’t see what their mind tells them isn’t there.",
+            author: "Terry Pratchett (Mort)")
+
+Quote.create(saying: "Belief is a force. It’s a weak force, by comparison with gravity; when it comes to moving mountains, gravity wins every time.",
+            author: "Terry Pratchett (Pyramids)")
+
+Quote.create(saying: "No one is more worried by the actual physical manifestation of a god than his priests; it’s like having the auditors in unexpectedly.",
+            author: "Terry Pratchett (Pyramids)")
+
+Quote.create(saying: "Just because fate throws you together doesn’t mean fate’s got it right.",
+            author: "Terry Pratchett (Pyramids)")
+
+Quote.create(saying: "It must be something about high office. The altitude sends people mad.",
+            author: "Terry Pratchett (Guards! Guards!)")
+
+Quote.create(saying: "He said that there was death and taxes, and taxes was worse, because at least death didn’t happen to you every year.",
+            author: "Terry Pratchett (Reaper Man)")
+
+Quote.create(saying: "Pets are always a great help in times of stress. And in times of starvation too, o’course.",
+            author: "Terry Pratchett (Small Gods)")
